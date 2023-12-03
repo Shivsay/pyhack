1707a26bd2d4887aabf1432c472077075d5e6b9e
Shivsay <shivsa543@gmail.com>
2023-12-04 00:51:23 +0530

    Updated README and made some api_helper changes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # pyhack
+A [HackerNews](https://news.ycombinator.com/) front-end application built using tkinter.
 
 pyhack uses [haxor](https://github.com/avinassh/haxor) as a python wrapper to access the HackerNews [API](https://github.com/HackerNews/API).
 
@@ -6,3 +7,9 @@ pyhack uses [haxor](https://github.com/avinassh/haxor) as a python wrapper to ac
 
 Go to the directory which contains pyhack, then run 
 `python -m pyhack.gui.mainGui`
+
+## Gallery
+
+![Front Page](/images/front_page.png)
+
+![Comments Page](/images/comments_page.png)
--- api/api_helper.py
+++ api/api_helper.py
@@ -4,23 +4,27 @@ from bs4 import BeautifulSoup
 
 hn = HackerNews()
 
-def getTop(id):
-    top = hn.get_item(id)
-    content = [top.url, top.text]
-    #content[0] = top.url
-    #content[1] = top.text
-    return content
 
-def getComment(id):
+def getComments(id):
      itemObj = hn.get_item(id)
      return itemObj.kids
 
 def parseComment(id):
     commentListItem = hn.get_item(id)
     commentHTMLItem = commentListItem.text
-    commentParsedItem = BeautifulSoup(commentHTMLItem, 'html.parser')
-    commentItem = commentParsedItem.get_text()
-    return commentItem
+    if commentHTMLItem:
+        commentParsedItem = BeautifulSoup(commentHTMLItem, 'html.parser')
+        commentItem = commentParsedItem.get_text()
+        return commentItem
+    else :
+        return ""           #leaves spaces?
+
+def getTop(id):
+    top = hn.get_item(id)
+    content = [top.url, top.text]
+    #content[0] = top.url
+    #content[1] = top.text
+    return content
 
 def getShowStories(): 
     showStories = hn.show_stories(limit=20)
@@ -31,10 +35,10 @@ def getTopStories():
     topStories = hn.top_stories(limit=20)
     return topStories
 
-def getjobStories():
+def getJobStories():
     jobStories = hn.job_stories(limit=20)
     return jobStories
 
-def getnewStories():
+def getNewStories():
     newStories = hn.new_stories(limit=20)
     return newStories
